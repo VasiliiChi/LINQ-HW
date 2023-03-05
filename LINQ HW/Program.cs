e60c3b601c3bac5689949048f2da04f491cbebe0
Vasilii <chibisowasya@gmail.com>
2023-03-05 21:49:49 +0300

    Добавьте файлы проекта.

---
+++ LINQ HW/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var classes = new[]
+        {
+            new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+            new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+            new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+        };
+        var allStudents = GetAllStudents(classes);
+
+        Console.WriteLine(string.Join(" ", allStudents));
+    }
+
+    static string[] GetAllStudents(Classroom[] classes)
+    {
+        // Решение
+        var studentsName = from cs in classes
+            from stud in cs.Students
+            select stud;
+        return  studentsName.ToArray();
+    }
+
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
